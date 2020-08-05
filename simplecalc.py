9cc9e32f08dc7de4272b32f8127d017b0d71f852
Syehino <32030125+Syehino@users.noreply.github.com>
2020-08-06 01:46:04 +0800

    Improved (?) the calculator
    Added validation for operation, added a function to calculate the input and added ability to rerun the program when finished

--- simplecalc.py
+++ simplecalc.py
@@ -1,26 +1,53 @@
-#Beginner app [Simple Calculator]
+#Beginner calculator
 
-operation = input("Select operation (+|-|*|/):")
+cont = "Y"
 
-#simple validation
-while True:
-    try:
-        num1 = int(input("Enter first number: "))
-        num2 = int(input("Enter second number: "))
-        break
-    except ValueError:
-        print("Please enter an interger")
-
-if operation == '+':
-    print("The sum of the input is: ", num1 + num2)
-
-if operation == '-':
-    print("The sum of the input is: ", num1 - num2)
-
-if operation == '*':
-    print("The sum of the input is: ", num1 * num2)
-
-if operation == '/':
-    print("The sum of the input is: ", num1 / num2)
+while cont == "Y" or cont == "y":
+ 
+    operation = input("Select operation (+|-|*|/):")
+    
+    while True:
+        if (operation == "+" or operation == "-" or operation == "*" or operation == "/"):
+            break
+        else:
+            operation = input("Please enter valid operation:")
+    
+    #simple validation
+    while True:
+        try:
+            num1 = int(input("Enter first number: "))
+            num2 = int(input("Enter second number: "))
+            break
+        except ValueError:
+            print("Please enter an interger")
+    
+    #Simple function
+    def result(numA, numB, op):
+        if op == '+':
+         print("The sum of the input is: ", numA + numB)
+    
+        if op == '-':
+         print("The substraction of the input is: ", numA - numB)
+    
+        if op == '*':
+         print("The multiplication of the input is: ", numA * numB)
+    
+        if op == '/':
+         print("The division of the input is: ", numA / numB)
+    
+    #Function call
+    result(num1, num2, operation)
+    
+    #Rerun calculator
+    cont = input("Do you want to run the calculator again? [Y/N] ")
+    
+    while True:
+        if cont == "y" or cont == "Y":
+            break
+        else:
+            print("Calculator stopped")
+            break
+        
+#Note: While True means that it will loop indefinitely until it is breaked (stopped) in the statement.
 
 
